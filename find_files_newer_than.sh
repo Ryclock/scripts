f0f3301a8dcae832b19286e4409f502cd9166512
ChildJ <rythme.clock@gmail.com>
2025-05-04 19:47:07 +0800

    add: lastest time fix: remove quote before&after maxdepth

--- find_files_newer_than.sh
+++ find_files_newer_than.sh
@@ -23,13 +23,21 @@ if [ "$recursive" = "false" ]; then
     maxdepth_option="-maxdepth 1"
 fi
 
-find "$source_dir" "$maxdepth_option" -type f -name "$file_pattern" -printf "%p:%TY-%Tm-%Td %TH:%TM:%TS\n" | while IFS= read -r line; do
-    file_path=$(echo "$line" | cut -d':' -f1)
+lastest_timestamp=0
+readarray -t lines < <(find "$source_dir" $maxdepth_option -type f -name "$file_pattern" -printf "%p:%TY-%Tm-%Td %TH:%TM:%TS\n")
+for line in "${lines[@]}"; do
+    file_path=$(echo "$line" | cut -d':' -f1 | sed 's|^\./||')
     file_time=$(echo "$line" | cut -d':' -f2- | cut -d'.' -f1)
 
     file_timestamp=$(date -d "$file_time" +%s 2>/dev/null)
 
     if [ -n "$file_timestamp" ] && [ "$file_timestamp" -gt "$start_timestamp" ]; then
-        echo "${file_path}:${file_time}"
+        echo "${file_path}: [作用], 更新时间${file_time}"
+    fi
+
+    if [ "$file_timestamp" -gt "$lastest_timestamp" ]; then
+        lastest_timestamp=$file_timestamp
     fi
 done
+
+echo "last timestamp:$(date -d @${lastest_timestamp} +'%Y-%m-%d %H:%M:%S')"
